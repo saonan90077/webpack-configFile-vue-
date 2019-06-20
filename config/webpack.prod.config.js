75262c165745ab287f6d5488613a9e749b752cde
M_Events <34467452+saonan90077@users.noreply.github.com>
2019-06-20 13:59:03 +0800

    Update webpack.prod.config.js

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -30,7 +30,7 @@ module.exports = merge(baseConfig, {
 	      }
 	    },
 	    runtimeChunk: {
-	    	name: "manifest "
+	    	name: "manifest"
 	    },
 	    minimizer: [
 	      new OptimizeCSSAssetsPlugin({})
